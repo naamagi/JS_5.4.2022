21f53f5480932c7cc104b1a5adeedaf26bb5008d
naamagi <n.gilat@gmail.com>
2022-04-05 23:26:04 +0300

    First commit

---
+++ 1.1-primitives_data_types.js
@@ -0,0 +1,16 @@
+const name1="Naama";
+const number1=123;
+const obj1=Math;
+const isClean=true;
+const loggedInUser=null;
+const undefined1=undefined;
+let mySymbol = Symbol("mySymbol");
+
+console.log(typeof(name1));
+console.log(typeof(number1));
+console.log(typeof(obj1));
+console.log(typeof(isClean));
+console.log(typeof(loggedInUser));
+console.log(typeof(undefined1));
+console.log(typeof(mySymbol));
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# JS_5.4.2022
+
+Version 1.0.0
+
+## Sample of Hotel website
+---
+Contributors
+-Naama G.
+---
+License & copyright
+© Naama G.
