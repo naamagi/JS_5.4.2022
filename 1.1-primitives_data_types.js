63e6dda43ffb299f40222ba7eed8396fccc46ce6
naamagi <n.gilat@gmail.com>
2022-04-06 07:52:13 +0300

    Update

--- 1.1-primitives_data_types.js
+++ 1.1-primitives_data_types.js
@@ -1,6 +1,6 @@
 const name1="Naama";
 const number1=123;
-const obj1=Math;
+const person = new Object();
 const isClean=true;
 const loggedInUser=null;
 const undefined1=undefined;
@@ -8,7 +8,7 @@ let mySymbol = Symbol("mySymbol");
 
 console.log(typeof(name1));
 console.log(typeof(number1));
-console.log(typeof(obj1));
+console.log(typeof(person));
 console.log(typeof(isClean));
 console.log(typeof(loggedInUser));
 console.log(typeof(undefined1));
